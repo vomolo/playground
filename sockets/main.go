6f526074a2381bc170ae889106e4a63960bb22fd
vomolo <valentineomolo1@gmail.com>
2025-05-26 11:41:51 +0300

    FEAT: Upgrader function

--- sockets/main.go
+++ sockets/main.go
@@ -11,3 +11,30 @@ var upgrader = websocket.Upgrader{
 		return true
 	},
 }
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "Could not upgrade connection", http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		messageType, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+
+		if err = conn.WriteMessage(messageType, msg); err != nil {
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic("Failed to start server: " + err.Error())
+	}
+}
